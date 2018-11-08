e1fc3493841d7accec2fa9cb369e2e7f0212bc03
danafisk <dfisk@mail.csuchico.edu>
2018-11-07 17:31:21 -0800

    yes!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+void colors(int&, int&, int&);
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  colors(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +24,52 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void colors(int &a, int &b, int &c)
+{
+  int temp;
+  
+  if (b > a && c > a && c > b) 
+  {
+    temp = a;
+    a = c;
+    c = temp;
+  }
+
+  if (b > a && b > c && c > a )
+
+  {
+    temp = a;
+    a = b;
+    b = temp;
+
+    temp = b;
+    b = c;
+    c = temp;
+  }
+
+  if (b > a && b > c && a > c)
+  {
+     temp = a;
+     a = b;
+     b = temp;
+  }
+
+  if (a > b && c > b && c > a)
+  {
+   temp = b;
+   b = c;
+   c = temp;
+
+   temp = a;
+   a = b;
+   b = temp;
+  }
+
+  if ( a > b && a > c && c > b)
+
+  {
+  temp = b;
+  b = c;
+  c = temp;
+  }
+      }
